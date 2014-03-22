01d04db7a297f6ad55c6a18963122398a446e834
Nathan Korzekwa <ciscprocess@gmail.com>
2014-03-22 01:07:56 -0400

    removed some old test-code and replaced it with proper gomoku code.

--- gomoku.py
+++ gomoku.py
@@ -16,25 +16,14 @@ import ai
 
 state = game.GameState()
 def main():
-    #graphics.clear()
-    #graphics.drawBoard(state)
-    #ui.mainloop()
-    money = 10
-    count = 0
-    for fives in xrange(3):
-        for fours in xrange(3):
-            for threes in xrange(4):
-                for twos in xrange(6):
-                    for ones in xrange(11):
-                        if (fives * 6 + fours * 4 + threes * 3 + twos * 2 + ones) == money:
-                            count += 1
-
-    print str(count)
-##    for i in xrange(10):
-##        for ii in xrange(10):
-##            state.play((i, ii), 'p')
-##            graphics.clear()
-##            graphics.drawBoard(state)
+    graphics.clear()
+    graphics.drawBoard(state)
+    ui.mainloop()
+    for i in xrange(10):
+        for ii in xrange(10):
+            state.play((i, ii), 'p')
+            graphics.clear()
+            graphics.drawBoard(state)
 
 if __name__ == '__main__':
     main()
